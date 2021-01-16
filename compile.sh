4cbea5747076d10879f1adcc23f80bc74f70ddcd
Driesboy <coenendries3@gmail.com>
2021-01-16 21:53:53 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -854,8 +854,7 @@ get_github_extension "ds" "$EXT_DS_VERSION" "php-ds" "ext-ds"
 
 get_github_extension "recursionguard" "$EXT_RECURSIONGUARD_VERSION" "pmmp" "ext-recursionguard"
 
-#get_github_extension "zstd" "$EXT_ZSTD_VERSION" "kjdev" "php-ext-zstd"
-get_pecl_extension "zstd" "$EXT_ZSTD_VERSION"
+get_github_extension "zstd" "$EXT_ZSTD_VERSION" "kjdev" "php-ext-zstd"
 
 echo -n "  crypto: downloading $EXT_CRYPTO_VERSION..."
 git clone https://github.com/bukka/php-crypto.git "$BUILD_DIR/php/ext/crypto" >> "$DIR/install.log" 2>&1
